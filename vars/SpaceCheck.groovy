2b6be57ea2be2e1ad057370467e01d7f3a133d8d
Arwa Ahmed <60405109+ArwaAhmed98@users.noreply.github.com>
2023-12-14 03:33:07 +0200

    Update SpaceCheck.groovy

--- vars/SpaceCheck.groovy
+++ vars/SpaceCheck.groovy
@@ -1,18 +1,17 @@
 def call() {
-    sh """#!/bin/bash
+     sh """#!/bin/bash
     df | awk '/\\/dev\\/disk*/ && ! /\\/private\\/var\\/vm/ {
-        s_byte = \$2 * 512            # covert blocks to bytes
+        s_byte = \$2 * 512            # convert blocks to bytes
         a_byte = \$4 * 512
-        ts_byte+=s_byte              # add bytes for each device
-        ta_byte+=a_byte              # to their total
+        ts_byte += s_byte            # add bytes for each device
+        ta_byte += a_byte            # to their total
     }
 
     END {
-        ts_byte=ts_byte/1000000000   # convert bytes to GB
-        ta_byte=ta_byte/1000000000
-        print \\"CAPACITY:\", ts_byte,\"G     AVAILABLE:\", ta_byte,\"G\"
+        ts_byte = ts_byte / 1000000000   # convert bytes to GB
+        ta_byte = ta_byte / 1000000000
+
+        print \"CAPACITY:\", ts_byte, \"G     AVAILABLE:\", ta_byte, \"G\"
     }'
     """
 }
-
-call()
